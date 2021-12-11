3261601fca6c5ed6bf5d4ab1e0f3bba30b7ef691
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-12-11 19:07:53 +0200

    Add files via upload

---
+++ src/Our_Proj/_EdgeData.java
@@ -0,0 +1,95 @@
+package Our_Proj;
+
+import api.EdgeData;
+
+public class _EdgeData implements EdgeData {
+    private  int Src;
+    private int Dest;
+    private double Weight;
+    private String Info;
+    private int Tag;
+    private static int e_caunt;
+
+
+
+   public _EdgeData(){
+       this.Src = 0;
+       this.Dest =0;
+       this.Weight =0.0;
+       this.Info = "";
+       this.Tag = 0;
+       e_caunt++;
+
+   }
+    public _EdgeData(_EdgeData other){
+        this.Src = other.Src;
+        this.Dest = other.Dest;
+        this.Weight = other.Weight;
+        this.Info = other.Info;
+
+    }
+   public _EdgeData(int s,int d,double w){
+       this.Src = s;
+       this.Dest = d;
+       this.Weight = w;
+      // this.edgeToNode = new HashMap<NodeData,EdgeData>();
+
+   }
+    public _EdgeData(int s,int d){
+        this.Src = s;
+        this.Dest = d;
+
+
+    }
+
+
+    //setters needed too
+    public void setWeight(double w){
+       this.Weight = w;
+    }
+    @Override
+    public int getSrc() {
+        return Src;
+    }
+
+    @Override
+    public int getDest() {
+        return Dest;
+    }
+
+    @Override
+    public double getWeight() {
+        return Weight;
+    }
+
+    @Override
+    public String getInfo() {
+        return Info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+    this.Info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return Tag;
+    }
+
+    @Override
+    public String toString() {
+        return "Our_Proj._EdgeData{" +
+                "Src=" + Src +
+                ", Dest=" + Dest +
+                ", Weight=" + Weight +
+                ", Info='" + Info + '\'' +
+                ", Tag=" + Tag +
+                '}';
+    }
+
+    @Override
+    public void setTag(int t) {
+     this.Tag = t;
+    }
+}
---
+++ src/Our_Proj/_RandomGraphsGenerator.java
@@ -0,0 +1,19 @@
+package Our_Proj;
+
+public class _RandomGraphsGenerator extends _DirectedWeightedGraph{
+    /**this method made for creating random graphs for test
+     * that going to be tested from main,this graph is needed for
+     * _RandowGraphsGenerator
+     * as said at DWG its should support 100,000 nodes graphs
+     * for each node in average 20 kids for each the edges are randomize
+     * becouse its directed we doing randomization for each node
+     * will make randomization for evrytime elsly the random node will be
+     * set to 0 and all other nodes should be set to infinity
+     * by the weights we track we making dessisions
+     * we extending Our_Proj._DirectedWeighedGraphAlgorithms we have everything there
+     * will make boolean for first node and hes value is 0 all others is infinity
+     *
+     */
+
+
+}
